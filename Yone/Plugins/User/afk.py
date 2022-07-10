d6aa577b462c2005fae6561a778d7a75cbf81347
ribfirst <108966423+ribfirst@users.noreply.github.com>
2022-07-11 02:55:14 +0300

    Update afk.py

--- Yone/Plugins/User/afk.py
+++ Yone/Plugins/User/afk.py
@@ -26,7 +26,7 @@ def afk(update: Update, context: CallbackContext):
     if not user:  # ignore channels
         return
 
-    if user.id in [777000, 1087968824]:
+    if user.id in [777000, 1008271006]:
         return
 
     notice = ""
